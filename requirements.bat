987f34ec45c09758e324ea10907eb6e84eba993f
VenzOT <venzot10@gmail.com>
2024-07-24 19:21:45 +0200

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,12 @@
+@echo off 
+title Setup Tools
+pip install colorama
+pip install pystyle
+pip install requests
+pip install psutil
+pip install cryptography
+pip install fade
+
+@echo off
+title Get Web IP by venz.db
+WebIP.py
